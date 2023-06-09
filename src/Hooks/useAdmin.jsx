0d93533f2f195ff6216c4a140e414e9235852d55
unknown <24abdullahalmasum@gmail.com>
2023-06-10 00:30:15 +0600

    added swal

--- src/Hooks/useAdmin.jsx
+++ src/Hooks/useAdmin.jsx
@@ -5,15 +5,17 @@ import { AuthContext } from "../Components/AuthProvider/AuthProvider";
 
 
 const useAdmin = () => {
-    const {user} = useContext(AuthContext)
-    const {data: isAdmin, isLoading: isAdminLoading} = useQuery({
+    const {user,loading} = useContext(AuthContext)
+    const {data: isAdmin, isLoading: isAdminLoading, refetch} = useQuery({
         queryKey: ['isAdmin'],
+        enabled: !loading,
         queryFn: async () => {
             const response = await axios.get(`http://localhost:5000/users/admin/${user?.email}`);
+
             return response.data.admin;
         }
     })
-    return [isAdmin, isAdminLoading]
+    return [isAdmin, isAdminLoading,refetch]
 };
 
 export default useAdmin;
