6655482299d87a1065fce101643180b2fa4ade30
unknown <24abdullahalmasum@gmail.com>
2023-06-10 14:43:54 +0600

    added modal

--- src/Pages/Dashboard/Admin/ManageClasses/ManageClasses.jsx
+++ src/Pages/Dashboard/Admin/ManageClasses/ManageClasses.jsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import Swal from 'sweetalert2';
-import { useState } from 'react';
+import axios from 'axios';
+import {
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  FormControl,
+  FormLabel,
+  Input,
+} from '@chakra-ui/react';
 import { FcApproval } from 'react-icons/fc';
 import { MdDoNotDisturb } from 'react-icons/md';
 import { BiMessageRoundedEdit } from 'react-icons/bi';
-
-import axios from 'axios';
 import useClasses from '../../../../Hooks/useClasses';
 
 const ManageClasses = () => {
   const [permit, setPermit] = useState([]);
   const [classes, , refetch] = useClasses();
+  const [isOpen, setIsOpen] = useState(false);
+  const [editingClass, setEditingClass] = useState(null);
+  const initialRef = useRef(null);
+  const finalRef = useRef(null);
 
   const handleApprove = (cla) => {
     if (cla.status === 'approve') {
@@ -22,6 +37,7 @@ const ManageClasses = () => {
       if (response.data.modifiedCount) {
         refetch();
         setPermit([...permit, cla._id]);
+        setEditingClass(cla);
         Swal.fire({
           position: 'top-end',
           icon: 'success',
@@ -43,6 +59,7 @@ const ManageClasses = () => {
       if (response.data.modifiedCount) {
         refetch();
         setPermit([...permit, cla._id]);
+        setEditingClass(cla);
         Swal.fire({
           position: 'top-end',
           icon: 'success',
@@ -69,86 +86,131 @@ const ManageClasses = () => {
     return permit.includes(claId);
   };
 
+  const handleOpenModal = (cla) => {
+    setIsOpen(true);
+    setEditingClass(cla);
+  };
+
+  const handleCloseModal = () => {
+    setIsOpen(false);
+    setEditingClass(null);
+  };
+
   return (
-    <table className="min-w-full divide-y divide-gray-200 pr-6">
-      <thead>
-        <tr>
-          <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-            class Image
-          </th>
-          <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-            class Name
-          </th>
-          <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-            Instructor
-          </th>
-          <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-            Email
-          </th>
-          <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-            Seats Available
-          </th>
-          <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-            Price
-          </th>
-          <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-            Status
-          </th>
-          <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-            Permit
-          </th>
-          <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-            Feedback
-          </th>
-        </tr>
-      </thead>
-      <tbody className="bg-white divide-y divide-gray-200">
-        {classes.map((cla) => (
-          <tr key={cla._id}>
-            <td className="px-6 py-4 whitespace-nowrap">
-              <img src={cla.image} alt={cla.name} className="h-8 w-8 rounded-full" />
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap">{cla.name}</td>
-            <td className="px-6 py-4 whitespace-nowrap">{cla.instructorName}</td>
-            <td className="px-6 py-4 whitespace-nowrap">{cla.instructorEmail}</td>
-            <td className="px-6 py-4 whitespace-nowrap">{cla.availableSeats}</td>
-            <td className="px-6 py-4 whitespace-nowrap">{cla.price}</td>
-            <td className="px-6 py-4 whitespace-nowrap">{cla.status}</td>
-            <td className="px-6 py-4 whitespace-nowrap">
-              {!isPermitted(cla._id) ? (
-                <div className="dropdown dropdown-left">
-                  <label tabIndex={0} className="btn m-1">
-                    permission
-                  </label>
-                  <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
-                    <li className="btn btn-sm" onClick={() => handleApprove(cla)}>
-                      {getPermitIcon('approve')} approve
-                    </li>
-                    <li className="btn btn-sm" onClick={() => handleDeny(cla)}>
-                      {getPermitIcon('deny')} deny
-                    </li>
-                  </ul>
-                </div>
-              ) : (
-                <span className="text-gray-500">
-                  <p className='flex justify-center'>
-                  <p>
-                  {cla.status === 'approved' && <FcApproval className="text-5xl" />}
-                  </p>
-                  </p>
-                  <p className='flex justify-center'>
-                    <p>
-                    {cla.status === 'denied' && <MdDoNotDisturb className="text-5xl" />}
-                    </p>
-                  </p>
-                </span>
-              )}
-            </td>
-            <td className="px-6 py-4 whitespace-nowrap text-4xl"><BiMessageRoundedEdit/></td>
+    <>
+      <table className="min-w-full divide-y divide-gray-200 pr-6">
+        <thead>
+          <tr>
+            <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              class Image
+            </th>
+            <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              class Name
+            </th>
+            <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Instructor
+            </th>
+            <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Email
+            </th>
+            <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Seats Available
+            </th>
+            <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Price
+            </th>
+            <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Status
+            </th>
+            <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Permit
+            </th>
+            <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Feedback
+            </th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody className="bg-white divide-y divide-gray-200">
+          {classes.map((cla) => (
+            <tr key={cla._id}>
+              <td className="px-6 py-4 whitespace-nowrap">
+                <img src={cla.image} alt={cla.name} className="h-8 w-8 rounded-full" />
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap">{cla.name}</td>
+              <td className="px-6 py-4 whitespace-nowrap">{cla.instructorName}</td>
+              <td className="px-6 py-4 whitespace-nowrap">{cla.instructorEmail}</td>
+              <td className="px-6 py-4 whitespace-nowrap">{cla.availableSeats}</td>
+              <td className="px-6 py-4 whitespace-nowrap">{cla.price}</td>
+              <td className="px-6 py-4 whitespace-nowrap">{cla.status}</td>
+              <td className="px-6 py-4 whitespace-nowrap">
+                {!isPermitted(cla._id) ? (
+                  <div className="dropdown dropdown-left">
+                    <label tabIndex={0} className="btn m-1">
+                      permission
+                    </label>
+                    <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                      <li className="btn btn-sm" onClick={() => handleApprove(cla)}>
+                        {getPermitIcon('approve')} approve
+                      </li>
+                      <li className="btn btn-sm" onClick={() => handleDeny(cla)}>
+                        {getPermitIcon('deny')} deny
+                      </li>
+                    </ul>
+                  </div>
+                ) : (
+                  <span className="text-gray-500">
+                    <div className='flex justify-center'>
+                      <div>
+                        {cla.status === 'approved' && <FcApproval className="text-5xl" />}
+                      </div>
+                    </div>
+                    <div className='flex justify-center'>
+                      <div>
+                        {cla.status === 'denied' && <MdDoNotDisturb className="text-5xl" />}
+                      </div>
+                    </div>
+                  </span>
+                )}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-4xl">
+                <BiMessageRoundedEdit onClick={() => handleOpenModal(cla)} />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <Modal
+        isOpen={isOpen}
+        onClose={handleCloseModal}
+        initialFocusRef={initialRef}
+        finalFocusRef={finalRef}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Create your account</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl>
+              <FormLabel>First name</FormLabel>
+              <Input ref={initialRef} placeholder="First name" />
+            </FormControl>
+
+            <FormControl mt={4}>
+              <FormLabel>Last name</FormLabel>
+              <Input placeholder="Last name" />
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3}>
+              Save
+            </Button>
+            <Button onClick={handleCloseModal}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
 
